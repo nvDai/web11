4eba0b28194b7ecba357e7ff80b6779d9b49552c
nvDai <nvDai@users.github.com>
2018-03-25 09:55:06 +0700

    Bai tap buoi 7

---
+++ B_07/routers/questionRouter.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const questionController = require("../controllers/questionController");
+
+router.get('/:id', (req,res) =>{
+    var id = req.params.id;
+    questionController.findID(id, (collection) => {
+            // let questionList = [ ...fileController.readFileSync('./data.json') ]; ///... để nối mảng
+            let question = collection;
+            let total = question.yes + question.no;
+            let yesRate = (question.yes * 100 / (question.yes + question.no)).toFixed(2);
+            let noRate = (question.no * 100 / (question.yes + question.no)).toFixed(2);
+            if(total == 0) {
+                yesRate = noRate = 50;
+            }
+            res.render('question',{
+            question: question.questionContent,
+            id: question.id,
+            yes: question.yes,
+            no: question.no,
+            totalVote: total,
+            noRate: noRate,
+            yesRate: yesRate,
+            activeQuestion: "active"
+        });
+    });
+    
+});
+
+
+
+module.exports = router;
